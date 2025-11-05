0f4a9496421943bea071d08c81e122cb8a4ada12
antoshka838 <anivanov1987788990012@gmail.com>
2025-11-06 02:51:18 +0300

    fix: transform array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../lib');
+const { NotImplementedError } = require("../lib");
 
 /**
  * Create transformed array based on the control sequences that original
@@ -14,32 +14,38 @@ const { NotImplementedError } = require('../lib');
  *
  */
 function transform(arr) {
-
-  if (!Array.isArray(arr)) throw new Error("'arr' parameter must be an instance of the Array!");
+  if (!Array.isArray(arr))
+    throw new Error("'arr' parameter must be an instance of the Array!");
 
   let newArr = [];
-  for (let i = 0; i < arr.length; i += 1){
-    if (arr[i] === '--discard-next') { 
-      i++;
-    } else if (arr[i] === '--discard-prev') { 
-      newArr.pop();
-    } else if (arr[i] === '--double-next') {
-      if (i + 1 < arr.length){
+  let skipFlag = false;
+
+  for (let i = 0; i < arr.length; i += 1) {
+    if (skipFlag) {
+      skipFlag = false;
+      continue;
+    }
+
+    if (arr[i] === "--discard-next") {
+      skipFlag = true;
+    } else if (arr[i] === "--discard-prev") {
+      if (i > 0 && arr[i - 2] !== '--discard-next') newArr.pop();
+    } else if (arr[i] === "--double-next") {
+      if (i + 1 < arr.length) {
         newArr.push(arr[i + 1]);
       }
-    } else if (arr[i] === '--double-prev') {
-      if (newArr.length > 0) {
-        newArr.push(newArr[newArr.length - 1]);
+    } else if (arr[i] === "--double-prev") {
+      if (i > 0 && arr[i - 2] !== '--discard-next') {
+        newArr.push(arr[i - 1]);
       }
     } else {
       newArr.push(arr[i]);
     }
-
   }
 
   return newArr;
 }
 
 module.exports = {
-  transform
+  transform,
 };
